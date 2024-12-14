42f350cd914652906a3e1a57cfe6a2b2bf1b6300
Chandan <chandan@wecreate.app>
2024-12-14 21:45:58 +0530

    Added carousel and all necessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,122 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import useBreakpoints from "./hooks/useBreakpoints";
+
+import styles from "./App.styles"
+
+const multiplier = 100;
+const offset = 30;
+
+interface IItems {
+  title: string;
+  description: string;
+  author: string;
+  image: string;
+}
+
+const props: IItems[] = [
+  {
+    title: "Title 1",
+    description: "test description",
+    author: "Test Author",
+    image: ""
+  },
+  {
+    title: "Title 2",
+    description: "test description",
+    author: "Test Author",
+    image: ""
+  },
+  {
+    title: "Title 3",
+    description: "test description",
+    author: "Test Author",
+    image: ""
+  }
+];
 
 function App() {
+  const [navigateCounter, setNavigateCounter] = useState(0);
+  const breakpoints = useBreakpoints();
+
+  const handleClick = useCallback((decrement?: boolean) => {
+    if (decrement) {
+      setNavigateCounter((prev) => prev !== 0 ? prev - 1 : prev);
+    } else {
+      setNavigateCounter((prev) => prev + 1);
+    }
+  }, []);
+
+  const transformCss = useCallback((index: number) => {
+    if (!index && navigateCounter) {
+      return "scale3d(0.3, 0.3, 0.3)";
+    }
+
+    if (index + 1 >= navigateCounter) {
+      const translatePercentage = index + 1 === navigateCounter ? index : navigateCounter;
+      const translateXValue = `calc(-${translatePercentage * multiplier}% - ${navigateCounter * offset}px)`;
+
+      return `translateX(${translateXValue}) ${index + 1 <= navigateCounter
+        ? "scale3d(0.3, 0.3, 0.3)"
+        : ""}`;
+    }
+
+    return "";
+  }, [navigateCounter]);
+
+  const carouselButtons = useMemo(() => {
+    if (breakpoints.MD) {
+      return (
+        <div className={styles.navButtonsContainer}>
+          <button
+            disabled={!navigateCounter}
+            className={`${!navigateCounter ? styles.disabledNavButton : ""} ${styles.carouselNavButton}`}
+            onClick={() => handleClick(true)}
+          >
+            Prev
+          </button>
+          <button
+            onClick={() => handleClick()}
+            className={
+              `${navigateCounter === props.length - 1 ? styles.disabledNavButton : ""} ${styles.carouselNavButton}`
+            }
+            disabled={navigateCounter === props.length - 1}
+          >
+            Next
+          </button>
+        </div>
+      );
+    }
+
+    return <></>;
+  }, [breakpoints.MD, handleClick, navigateCounter]);
+
+  useEffect(() => {
+    if (!breakpoints.MD) {
+      setNavigateCounter(0);
+    }
+  }, [breakpoints.MD]);
 
   return (
-    <h1>
-      Hello
-    </h1>
+    <div className={styles.mainContainer}>
+      <h2 className={styles.textWhite}>Nav Buttons and card animation appear when screen size is less than 768</h2>
+      <div className={styles.carouselContainer}>
+        {props.map((item, index) => (
+          <div
+            key={index}
+            className={`${styles.carouselItem} ${index + 1 <= navigateCounter ? styles.prevCarouselItem : ""}`}
+            style={{ transform: transformCss(index) }}
+          >
+            <div className={styles.imageContainer}>
+
+            </div>
+            <h3 className={styles.itemTitle}>{item.title}</h3>
+            <p className={styles.itemDesc}>{item.description}</p>
+          </div>
+        ))}
+      </div>
+      {carouselButtons}
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: "#6b7280",
+      }
+    },
+  },
+  plugins: [],
+}
